b7b13edc01aae4421abb44859eb73a1d6e36614f
bijaya-dulal <dulalbijaya0@gmail.com>
2024-05-21 23:39:29 +0545

    login views updated

--- videoconferencing/call_app/views.py
+++ videoconferencing/call_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
+    if request.method == 'posT':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'login.html',{'success':"registration successful.please login"})
+        else:
+            error_messege = form.errors.as_text()
+            return render(request, 'register.html',{'success':error_messege})
+
     return render(request,'register.html')
 
 def login (request):
